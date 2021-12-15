da66bfaf0584955176c9e4086ee9b0ec62e5dd7e
hugo-fsd <62383769+hugo-fsd@users.noreply.github.com>
2021-12-15 09:01:37 +0000

    added login req to show info

--- js/user.js
+++ js/user.js
@@ -82,8 +82,33 @@ function validaLogin() {
       localStorage.setItem("token", token);
       document.getElementById("statusLogin").innerHTML = "Sucesso!";
       document.getElementById("btnLoginClose").click();
+      getInfo()
     })
     .catch(async (error) => {
       statLogin.innerHTML = error
     });
+}
+
+function getInfo() {
+  $('#myTable').DataTable({
+    ajax: {
+        url: '/api/gpus',
+        dataSrc: ""
+    },
+
+    columns:
+    [
+      {data: "gpuName"},
+      {data: "price"},
+      {data: "sellerName"}, 
+      {data: "url"}
+      
+    ],
+
+    columnDefs:
+    [{
+        targets: 3,
+        render: $.fn.dataTable.render.hyperLink("Go to Product Page")      
+    }]
+});
 }
